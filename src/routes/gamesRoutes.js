29d7cfdb69430a2cb65744af0458fe4b06698671
luzialeticia <leticialuzia@tutamail.com>
2020-10-06 12:03:19 -0300

    Criando a rota que chama todos os games

--- src/routes/gamesRoutes.js
+++ src/routes/gamesRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require("../controller/gamesController")
 const cors = require("cors")
 
+router.get("/games", cors(), controller.getGames)
 
 module.exports = router
