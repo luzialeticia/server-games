b3d85978829d96c5917d826d5ed81f4064b383ff
luzialeticia <leticialuzia@tutamail.com>
2020-10-06 12:06:49 -0300

    Função para buscar jogo pelo id

--- src/controller/gamesController.js
+++ src/controller/gamesController.js
@@ -4,6 +4,17 @@ const getGames = (request, response) => {
   response.status(200).send(games)
 }
 
+const getGamesById = (request, response) => {
+  const id = request.params.id
+  const game = games.find(game => game.id == id)
+  if (game) {
+      response.status(200).send(game)
+  } else {
+      response.status(404).send("Jogo não encontrado.")
+  }
+}
+
 module.exports = {
-  getGames
+  getGames,
+  getGamesById
 }
