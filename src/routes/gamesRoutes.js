f13ac20297498e7b065bf09dbae069f91a077b50
luzialeticia <leticialuzia@tutamail.com>
2020-10-06 11:53:35 -0300

    Comentando a linha do controller

--- src/routes/gamesRoutes.js
+++ src/routes/gamesRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const controller = require("../controller/musicasController")
+//const controller = require("../controller/")
 const cors = require("cors")
 
 
